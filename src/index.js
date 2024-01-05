7f5e251528727de2cc1de45f844f2fae3c8de2dd
MatthewLeaman <leaman.30@osu.edu>
2024-01-05 18:16:39 -0500

    complete chalenge lab with added delete all button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    handleJob(e.target.new_task_description.value)
+    form.reset()
+  })
 });
+
+function handleJob (newList) {
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', handleX);
+  btn.textContent = 'x';
+  p.textContent = newList + ' ';
+  p.appendChild(btn);
+  if (newList != "" && newList != " " && newList != "  ") {
+      document.querySelector('#tasks').appendChild(p);
+  }
+}
+
+function handleX (e) {
+  e.target.parentNode.remove();
+}
